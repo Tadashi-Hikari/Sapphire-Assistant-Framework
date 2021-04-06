fc5f5e5c7a86a72526d81ed20b1b17feb5ff3934
Christopher Carroll <ccarroll4812@gmail.com>
2021-04-05 21:07:14 -0400

    Just a status update. Wanted github to reflect that I am working

--- CalendarSkill/src/main/java/com/example/calendarskill/CalendarModuleInstallServiceRefinedTwo.kt
+++ CalendarSkill/src/main/java/com/example/calendarskill/CalendarModuleInstallServiceRefinedTwo.kt
@@ -2,7 +2,10 @@ package com.example.calendarskill
 
 import android.content.Intent
 import android.net.Uri
+import android.os.IBinder
 import com.example.componentframework.SapphireFrameworkRegistrationService
+import java.io.File
+import java.io.FileInputStream
 import java.io.FileOutputStream
 import java.io.OutputStream
 import java.net.URI
@@ -11,32 +14,117 @@ class CalendarModuleInstallServiceRefinedTwo: SapphireFrameworkRegistrationServi
     val VERSION = "0.0.1"
     val CONFIG = "calendar.conf"
     val fileList = arrayListOf<String>("get.intent","set.intent")
-    var REQUEST_FILE = "ACTION_SAPPHIRE_REQUEST_FILE"
+    var ACTION_MANIPULATE_FILEDATA = "action.framework.module.MANIPULATE_FILE_DATA"
+    var ACTION_REQUEST_FILEDATA = "action.framework.module.REQUEST_FILE_DATA" +
+            "DATA"
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
         Log.i(this.javaClass.name,"Calendar intent received")
         when(intent?.action){
-            ACTION_SAPPHIRE_MODULE_REGISTER -> registerModule(intent!!)
-            //ACTION_SAPPHIRE_CORE_REQUEST_DATA -> sendRequestedFiles(intent)
-            REQUEST_FILE -> demoRequestFile(intent)
+            ACTION_SAPPHIRE_MODULE_REGISTER -> registerModule(intent)
+            ACTION_MANIPULATE_FILEDATA -> coreTransferFile(intent)
         }
         return super.onStartCommand(intent, flags, startId)
     }
 
-    fun demoRequestFile(intent: Intent?){
-        // Bounce: ACTION_P2P_URI
-        // Transfer to Core: ACTION_CORE_TRANSFER
-        // File/Content Provider: ACTION_CUSTOM
-
+    override fun registerModule(intent: Intent) {
+        super.registerModule(intent)
     }
 
-    // I can implement this easy
-    fun coreTransferFile(){
-        var fileStream: FileOutputStream? = null
-        fileStream?.write(0)
+    fun coreTransferFile(intent: Intent){
+        try{
+            when(intent.hasExtra(DATA_KEYS)){
+                true -> processFiles(intent)
+                false -> Log.d(this.javaClass.name, "There was some kind of DATA_KEY error")
+            }
+        }catch(exception: Exception){
+            Log.d(this.javaClass.name, "Exception. There was some kind of DATA_KEY error")
+            Log.d(this.javaClass.name, exception.toString())
+        }
     }
 
-    // Same. Should they be moved to the back?
+    // This seems like unneeded modularity
+    fun processFiles(intent: Intent){
+        var clipData = intent.clipData!!
+        var firstUri = intent.data!!
+
+        writeToCore(firstUri)
+        for(clipIndex in 0..clipData.itemCount){
+            // This is how it has to be done w/ clipData it seems
+            writeToCore(clipData.getItemAt(clipIndex).uri)
+        }
+    }
+
+    fun writeToCore(uri: Uri){
+        try {
+            Log.i(this.javaClass.name,uri.toString()!!)
+            //var testFile = uri.toFile()
+            var somethingFD = contentResolver.openFileDescriptor(uri,"wa")!!
+            var fd = somethingFD.fileDescriptor
+            var outputStream = FileOutputStream(fd)
+            outputStream.write(". This is appended".toByteArray())
+            Log.i(this.javaClass.name,"Did it write?")
+
+            // This is the essential part, when it comes to editing a file
+            somethingFD = contentResolver.openFileDescriptor(uri,"rw")!!
+            fd = somethingFD.fileDescriptor
+            var inputStream = FileInputStream(fd)
+
+            var testFile = File(cacheDir,"temp")
+            var fileWriter = testFile.outputStream()
+
+            var data = inputStream!!.read()
+            while(data != -1){
+                fileWriter.write(data)
+                data = inputStream.read()
+            }
+            fileWriter.close()
+
+            Log.i(this.javaClass.name, testFile.readText())
+
+            Log.i(this.javaClass.name, "This seems like a valid way to edit the file")
+        }catch (exception: Exception){
+            Log.d(this.javaClass.name, "You cannot access the file this way")
+            Log.i(this.javaClass.name, exception.toString())
+        }
+    }
+
+    fun demoRequestFile(intent: Intent){
+        var uri = intent.data!!
+        try {
+            var uri = intent.data!!
+            Log.i(this.javaClass.name,uri.toString()!!)
+            //var testFile = uri.toFile()
+            var somethingFD = contentResolver.openFileDescriptor(uri,"wa")!!
+            var fd = somethingFD.fileDescriptor
+            var outputStream = FileOutputStream(fd)
+            outputStream.write(". This is appended".toByteArray())
+            Log.i(this.javaClass.name,"Did it write?")
+
+            // This is the essential part, when it comes to editing a file
+            somethingFD = contentResolver.openFileDescriptor(uri,"rw")!!
+            fd = somethingFD.fileDescriptor
+            var inputStream = FileInputStream(fd)
+
+            var testFile = File(cacheDir,"temp")
+            var fileWriter = testFile.outputStream()
+
+            var data = inputStream!!.read()
+            while(data != -1){
+                fileWriter.write(data)
+                data = inputStream.read()
+            }
+            fileWriter.close()
+
+            Log.i(this.javaClass.name, testFile.readText())
+
+            Log.i(this.javaClass.name, "This seems like a valid way to edit the file")
+        }catch (exception: Exception){
+            Log.d(this.javaClass.name, "You cannot access the file this way")
+            Log.i(this.javaClass.name, exception.toString())
+        }
+    }
+
     fun p2pFile(): Uri?{
         var uri = null
         return uri
@@ -46,7 +134,7 @@ class CalendarModuleInstallServiceRefinedTwo: SapphireFrameworkRegistrationServi
         // Implemented by developer
     }
 
-    inner class SapphireFile(){
-
+    override fun onBind(intent: Intent?): IBinder? {
+        return super.onBind(intent)
     }
 }
