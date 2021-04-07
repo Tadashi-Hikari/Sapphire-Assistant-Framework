c86e8fb3405f157f6acf8d3cef8b6929ecb4789e
Christopher Carroll <ccarroll4812@gmail.com>
2021-04-07 19:15:36 -0400

    Working on MultiprocessServiceRefined.kt

--- MultiprocessModule/src/main/java/com/example/multiprocessmodule/MultiprocessServiceRefined.kt
+++ MultiprocessModule/src/main/java/com/example/multiprocessmodule/MultiprocessServiceRefined.kt
@@ -6,6 +6,7 @@ import android.content.ServiceConnection
 import android.os.IBinder
 import android.os.SystemClock
 import com.example.componentframework.SapphireFrameworkService
+import org.json.JSONArray
 import org.json.JSONObject
 import java.io.File
 import kotlin.math.absoluteValue
@@ -14,25 +15,15 @@ import kotlin.random.Random
 /*
 This also can be used to wait for a response for a single intent. Perhaps it should have a different name
 
+The biggest difference between this, and how something like the CoreRegistrationService works is that
+the CoreRegistrationService is idle until the last intent returns. This one is meant to to processing
+in parallel, and as such has a different kind of complexity.
+
 I believe this may *have* to bind CoreService, now that Uris are the only way to pass around information
 I can't really record it to a JSON file the same way.
  */
 
 class MultiprocessServiceRefined: SapphireFrameworkService(){
-	// This ID is used to identify when a multiprocess intent is returning
-	val MULTIPROCESS_ID = "assistant.framework.multiprocess.protocol.ID"
-	// This is the count of multiprocess intents that still need to return for a specific ID
-	val SEQUENCE_NUMBER = "assistant.framework.multiprocess.protocol.SEQUENCE_NUMBER"
-	// This is the total amount of multiprocess intents under an ID
-	val SEQUENCE_TOTAL = "assistant.framework.multiprocess.column.SEQUENCE_TOTAL"
-
-	val MULTIPROCESS_TABLE = "multiprocess.tbl"
-
-	lateinit var databaseFile: File
-	lateinit var JSONDatabase: JSONObject
-	var connection = Connection()
-	// This will be the temp JSON replacement
-	var intentStorage = mutableListOf<Intent>()
 
 	// This ties the Multiprocess to Core much more than I wanted. Perhaps I should specify 'levels' of modules
 	inner class Connection() : ServiceConnection {
@@ -45,10 +36,17 @@ class MultiprocessServiceRefined: SapphireFrameworkService(){
 		}
 	}
 
+	// This could be replaced w/ core ID
+	val MULTIPROCESS_ID = "assistant.framework.multiprocess.protocol.ID"
+	// This holds the value while waiting for all returns
+	var storedIntents = mutableListOf<Intent>()
+	// This holds the relavent indexes for each ID
+	var activeIntents = JSONObject()
+	var connection = Connection()
+
 	override fun onCreate(){
 		var coreIntent = Intent().setClassName(this.packageName,"com.example.sapphireassistantframework.CoreService")
 		bindService(coreIntent,connection,0)
-		JSONDatabase = loadDatabase()
 		super.onCreate()
 	}
 
@@ -61,163 +59,74 @@ class MultiprocessServiceRefined: SapphireFrameworkService(){
 		return super.onStartCommand(intent, flags, startId)
 	}
 
-	fun evaluateMultiprocessIntent(intent: Intent){
-		when{
-			//value != 1 -> updateAndWait(intent)
-			//value == 1 -> sendFinalData(intent)
+	// Do what must be done
+	fun intializeMultiprocessIntent(intent: Intent?){
+		var multiprocessIntent = Intent(intent)
+		// Give it an ID for tracking
+		multiprocessIntent = generateId(intent)
+		// Add the initial intent and its data to storage, to await all results
+		activeIntents.put(multiprocessIntent.getStringExtra(MULTIPROCESS_ID),multiprocessIntent)
+		var independentRoutes = loadMultipleRoutes
+
+	}
+
+	fun loadMultipleRoutes(intent: Intent?){
+		var preparedIntent = prepareIntent(intent!!)
+		for(route in preparedIntent.getStringArrayListExtra("MULTIPROCESS_ROUTE_LIST")!!){
+			var packageClass = route.split(";")
+			preparedIntent.setClassName(packageClass[0],packageClass[1])
 		}
 	}
 
-	fun intializeMultiprocessIntent(intent: Intent){
-		var id = getNewID()
-		var routePair = expandMultiprocessRoute(intent)
-
-		intent.putExtra(MULTIPROCESS_ID, id)
-		// This is just for easy recording. The Sequence number gets overwritten by each local intent
-		intent.putExtra(SEQUENCE_NUMBER,routePair.first.size)
-
-		for(route in routePair.first.withIndex()){
-			dispatchMultiIntent(route,intent)
-		}
+	// This is just a convenience method to help make things more readable
+	fun prepareIntent(intent: Intent): Intent{
+		var preparedIntent = Intent(intent)
+		preparedIntent = regexRouteString(preparedIntent)
+		preparedIntent = makeMultiprocessList(preparedIntent)
+		// Make sure that every module knows what's going on
+		preparedIntent.setAction(ACTION_REQUEST_FILE_DATA)
+		// Know where to send it back when its done
+		preparedIntent.putExtra(ROUTE,)
+		return preparedIntent
 	}
 
-	// I think this is recording the information before its first dispatch. Need to double check
-	fun makeIntentRecords(intent: Intent) {
-		var intentId = intent.getIntExtra(SEQUENCE_NUMBER, -1).toString()
-		var activeMultiprocessesTable = loadTable(MULTIPROCESS_TABLE)
-
-		// If the ID doesn't exist, then record it in the table. Why did I record check this?
-		if(activeMultiprocessesTable.isNull(intentId)){
-
-		}
-
-		intentStorage.add(intent)
-		var storageId = intentStorage.size
-
-		var record = JSONDatabase.getJSONArray(intent.getStringExtra(MULTIPROCESS_ID))
-		record.put(storageId)
-		// I don't think I need to write anything, since this will be bound
-		JSONDatabase.put(MULTIPROCESS_ID,record)
-	}
-
-	// What is the point of this?
-	fun checkDuplicates(intent: Intent) {
-		var sequenceNumber = intent.getIntExtra(SEQUENCE_NUMBER, -1).toString()
-		var JSONMultiprocessRecord = loadTable(MULTIPROCESS_TABLE)
-		if (JSONMultiprocessRecord.isNull(sequenceNumber)) {
-			// Then it is unique. Save the data
-			var JSONIntentRecord = JSONObject()
-			// make the intent record
-			JSONIntentRecord.put(DATA_KEYS, intent.getStringArrayListExtra(DATA_KEYS))
-			Log.i(
-					"MultiprocessService",
-					"Getting DATA_KEYS from intent. Keys are ${intent.getStringArrayListExtra(DATA_KEYS)}"
-			)
-			for (dataKey in intent.getStringArrayListExtra(DATA_KEYS)!!) {
-				Log.i(
-						"MultiprocessService",
-						"Logging key ${dataKey}, value ${intent.getStringArrayListExtra(dataKey)}"
-				)
-				//Bold to assume that it will only ever be data keys
-				// I need to do something to account for duplicate names
-				JSONIntentRecord.put(dataKey, intent.getStringArrayListExtra(dataKey))
-			}
-			// save the intent record with its sequence number as its unique ID
-			var id = intent.getStringExtra(MULTIPROCESS_ID)
-			// Should I change anything about how this is saved?
-			JSONMultiprocessRecord.put(id + sequenceNumber, JSONIntentRecord)
-			JSONDatabase.put(id, JSONMultiprocessRecord)
-		}
-	}
-
-	// This dispatches ALL intents related to the multiprocess
-	fun dispatchMultiIntent(route: IndexedValue<String>,intent: Intent){
-		// I'd like to move this up a level, if I can
-		makeIntentRecords(intent)
-		var module = route.value.split(";")
-
-		var outgoingIntent = Intent()
-		// Dispatch it to one of the multiple locations listed
-		outgoingIntent.setClassName(module.component1(),module.component2())
-		outgoingIntent.putExtra(SEQUENCE_NUMBER, route.index)
-		// Core should redirect it here. Am I overwriting? or appending
-		outgoingIntent.putExtra(ROUTE,"${this.packageName};${this.javaClass.name}")
-		startService(outgoingIntent)
-	}
-
-	// This is a specialized parser for MultiProcess Intent
-	fun expandMultiprocessRoute(intent: Intent): Pair<List<String>,String>{
+	// This is unique to the Multiprocess Module. I need it to look for the unique () syntax
+	fun regexRouteString(intent: Intent): Intent{
 		var route = intent.getStringExtra(ROUTE)!!
-		Log.v(this.javaClass.name,"routeParser received this for its route: ${route}")
+		// Break out the multiprocess syntax
 		var start = route.indexOf("(")+1
 		var end = route.indexOf(")",start)
-		var multiprocessRouteString = route.substring(start,end)
-		var multiprocessRoute = multiprocessRouteString.split(',')
-		var remainingRoute = route.substring(end+1)
-		// I don't think I want to leave this returning a pair
-		return Pair(multiprocessRoute,remainingRoute)
+		// Return the multiprocess block
+		var multiprocessRoute = route.substring(start,end)
+		// Return the rest of the information
+		var remainingRoute = route.substring(end+1,)
+
+		// I think it's just easier to pass around the intent right now
+		intent.putExtra("MULTIPROCESS_ROUTE",multiprocessRoute)
+		intent.putExtra(ROUTE, remainingRoute)
+		return intent
 	}
 
-	fun sendFinalData(intent: Intent){
-		var id = intent.getStringExtra(MULTIPROCESS_ID)
-		Log.v(this.javaClass.name,"All data for MULTIPROCESS_ID ${id} received")
-		// The multiprocess record name is the multiprocess ID
-		// may need to read the file instead
-		var JSONMultiprocessRecord = JSONDatabase.getJSONObject(id)
-		var dataKeys = arrayListOf<String>()
-		var outgoingIntent = Intent()
-		// This is taking it from the last intent. This WILL cause an error where only one intents keys are gotten
-		outgoingIntent.putStringArrayListExtra(DATA_KEYS, intent.getStringArrayListExtra(DATA_KEYS)!!)
-
-		// This is changing. each JSONRecord will have a PRIMARY_KEY category, which I'll just check for
-		for(key in JSONMultiprocessRecord.keys()){
-			// Load the record for the key
-			if(key.contains(id.toString())){
-				var JSONIntentRecord = JSONMultiprocessRecord.getJSONObject(key)
-				var dataKeyString = JSONIntentRecord.getString(DATA_KEYS)
-				dataKeyString = dataKeyString.substring(1,dataKeyString.length-1)
-				dataKeys.addAll(dataKeyString.split(","))
-				for(dataKey in dataKeys){
-					outgoingIntent.putExtra(dataKey.trim(),JSONIntentRecord.getString(dataKey.trim()))
-				}
-				// Do I need to copy any data other than the DATA_KEYs? I may need to
-			}else{
-				// Copy whatever original data was associated w/ the intent
-				outgoingIntent.putExtra(key,JSONMultiprocessRecord.getString(key))
-			}
-		}
-		//outgoingIntent.putExtra(ROUTE,JSONMultiprocessRecord.getString(ROUTE))
-		//outgoingIntent.putExtra(MESSAGE,message)
-		// This needs to be accounted for, not hardcoded. It would just be the next in the pipeline
-		// proper
-		outgoingIntent.setClassName(this,"com.example.processormodule.ProcessorTrainingService")
-		startService(outgoingIntent)
-		// I don't know if this will work
-		if(JSONDatabase.keys() == null) {
-			// If there are no more multiprocess intents, shut down
-			unbindService(connection)
-			// could I clean this up a bit?
-			SystemClock.sleep(500)
-			stopSelf()
-		}
+	// This simply takes the multiprocess route string and turns it in to a list
+	fun makeMultiprocessList(intent: Intent): Intent{
+		var preparedIntent = Intent(intent)
+		var routeList: ArrayList<String> = preparedIntent.getStringExtra("MULTIPROCESS_ROUTE")!!.split(",") as ArrayList<String>
+		preparedIntent.putExtra("MULTIPROCESS_ROUTE_LIST", routeList)
+		return preparedIntent
 	}
 
-	// This could probably be simplified?
-	fun getNewID(): String{
+	fun generateId(intent: Intent?): Intent{
 		var id = -1
 		do{
 			id = Random.nextInt().absoluteValue
 		}while(id == -1)
-		return id.toString()
+
+		intent!!.putExtra(MULTIPROCESS_ID,id)
+		return intent
 	}
 
-	fun loadDatabase(): JSONObject{
-		// This seems... like it may not need to be formatted this way
-		databaseFile = File(filesDir, MULTIPROCESS_TABLE)
-		when(databaseFile.exists()) {
-			true -> return JSONObject(databaseFile.readText())
-			false -> return JSONObject()
-		}
+	fun evaluateMultiprocessIntent(intent: Intent?){
+
 	}
 
 	override fun onBind(intent: Intent?): IBinder? {
